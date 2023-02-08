22d27377a5c9117f006a673f38fe3561f8fc55d3
ivantsarkov <ivan.tsarkov.it@gmail.com>
2023-02-08 11:58:51 +0300

    sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -9,7 +9,9 @@ const AppRouter = () => {
         {Object.values(routeConfig).map(({element, path}) => (
           <Route
             key={path}
-            element={element}
+            element={(<div className="page-wrapper">
+              {element}
+            </div>)}
             path={path}
           />
         ))}
