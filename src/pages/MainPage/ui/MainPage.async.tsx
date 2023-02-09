aa67d0d28ec1e3a8d59a74619c142055b7cc7c90
ivantsarkov <ivan.tsarkov.it@gmail.com>
2023-02-09 19:12:25 +0300

    todo

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 
 export const MainPageAsync = lazy(async () => await new Promise(resolve => {
-  // @ts-expect-error
+  // @ts-expect-error todo: will fix
   setTimeout(() => { resolve(import('./MainPage')); }, 1500);
 }));
