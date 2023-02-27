42fe9f56b40119c37b6e827d8b9052a5dc662310
ivantsarkov <ivan.tsarkov.it@gmail.com>
2023-02-27 15:23:49 +0300

    slice

--- src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,3 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './LoginModal.module.scss';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
 
@@ -11,7 +9,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} className={classNames(cls.LoginModal, {}, [className])}>
+    <Modal isOpen={isOpen} onClose={onClose}>
       <LoginForm />
     </Modal>
   );
